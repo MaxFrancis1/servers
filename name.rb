3ac80e4a42961f47c771692b604225002bdc02ab
Max <max.francis2000@gmail.com>
2018-10-15 13:11:12 +0000

    refactored the code

--- name.rb
+++ name.rb
@@ -1,22 +1,17 @@
 require 'socket'
 
 server = TCPServer.new(2345)
-
+  socket = server.accept
 loop do
   
-  socket = server.accept
-
-
   socket.puts "What do you say?"
 
   they_said = socket.gets.chomp
 
   if they_said == "quit"
-    break
-  end
-
-  socket.puts "You said: #{they_said}. Goodbye!"
-
+    socket.close
+  else
   
+    socket.puts "You said: #{they_said}"
+  end
 end
-socket.close
